9d9143e574abe98b66de4031e73f67457368ce13
Daniel Lopes <daniel@lopes.ms>
2023-04-22 08:28:55 -0400

    teste 2

---
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,17 @@
+public class TiposVariaveis {
+
+    public static void main(String[] args ) {
+
+        String meuNome = "Daniel Lopes Filho";
+
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+
+        short numeroCurto2 = (short) numeroNormal;
+// hghghghg
+        int numero = 5;
+    }
+    
+}
